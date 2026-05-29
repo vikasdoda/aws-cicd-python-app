2f3fd2458ac8350a5c18fd7f7f89a0b9e9885a74
Vikas Doda <vikas.doda@gmail.com>
2026-05-29 17:33:43 +0200

    added missing files

---
+++ tests/test_app.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+
+from app.main import app
+
+client = TestClient(app)
+
+
+def test_home():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello, World from AWS CI/CD !"}
